c05c6c5f29cb8cd43b00a8b1bff784b9917c386a
DEVELOPMENTVM\benjamim <benjamim_pitacho@hotmail.com>
2017-05-28 19:51:50 +0100

    * Change: setup users management grid view with sort and search.

--- InsuranceSocialNetwork/InsuranceSocialNetworkDatabase/InitData/AspNetUsers.sql
+++ InsuranceSocialNetwork/InsuranceSocialNetworkDatabase/InitData/AspNetUsers.sql
@@ -44,23 +44,186 @@ VALUES
 	N'admin@falarseguros.pt'
 )
 
-INSERT [dbo].[AspNetUserRoles] (
-	RoleId,
-	UserId
-)
-VALUES
+INSERT [dbo].[AspNetUserRoles] (RoleId,UserId) VALUES ('1',@userId)
+
+INSERT [Insurance].[Profile]([ID_User],[FirstName]) VALUES (@userId,N'Administrator')
+
+SET @userId = CONVERT(nvarchar(128), NEWID());
+
+INSERT [dbo].[AspNetUsers] (
+	[Id], 
+	[AccessFailedCount],
+	[Email], 
+	[EmailConfirmed], 
+	[LockoutEnabled], 
+	[LockoutEndDateUtc],  
+	[PasswordHash], 
+	[PhoneNumber], 
+	[PhoneNumberConfirmed], 
+	[SecurityStamp], 
+	[TwoFactorEnabled], 
+	[UserName]
+) 
+VALUES 
 (
-	'1',
-	@userId
+	@userId, 
+	0, 
+	N'user1@falarseguros.pt', 
+	1, 
+	1, 
+	NULL, 
+	N'APyxnVe8q6Jg5S6N4QpDb8OYU5ommCD2rxv5NSXMRGvjdkSWZ9CZNT9Dr6/7ejADqQ==', 
+	NULL, 
+	0, 
+	N'c85f3301-03a8-42f1-b190-9d8469f1b652', 
+	0, 
+	N'user1@falarseguros.pt'
 )
 
-INSERT [Insurance].[Profile]
+INSERT [dbo].[AspNetUserRoles] (RoleId,UserId) VALUES ('2',@userId)
+
+INSERT [Insurance].[Profile]([ID_User],[FirstName]) VALUES (@userId,N'User1')
+
+SET @userId = CONVERT(nvarchar(128), NEWID());
+
+INSERT [dbo].[AspNetUsers] (
+	[Id], 
+	[AccessFailedCount],
+	[Email], 
+	[EmailConfirmed], 
+	[LockoutEnabled], 
+	[LockoutEndDateUtc],  
+	[PasswordHash], 
+	[PhoneNumber], 
+	[PhoneNumberConfirmed], 
+	[SecurityStamp], 
+	[TwoFactorEnabled], 
+	[UserName]
+) 
+VALUES 
 (
-	[ID_User],
-	[FirstName]
+	@userId, 
+	0, 
+	N'user2@falarseguros.pt', 
+	1, 
+	1, 
+	NULL, 
+	N'APyxnVe8q6Jg5S6N4QpDb8OYU5ommCD2rxv5NSXMRGvjdkSWZ9CZNT9Dr6/7ejADqQ==', 
+	NULL, 
+	0, 
+	N'c85f3301-03a8-42f1-b190-9d8469f1b652', 
+	0, 
+	N'user2@falarseguros.pt'
 )
-VALUES
+
+INSERT [dbo].[AspNetUserRoles] (RoleId,UserId) VALUES ('2',@userId)
+
+INSERT [Insurance].[Profile]([ID_User],[FirstName]) VALUES (@userId,N'User2')
+
+SET @userId = CONVERT(nvarchar(128), NEWID());
+
+INSERT [dbo].[AspNetUsers] (
+	[Id], 
+	[AccessFailedCount],
+	[Email], 
+	[EmailConfirmed], 
+	[LockoutEnabled], 
+	[LockoutEndDateUtc],  
+	[PasswordHash], 
+	[PhoneNumber], 
+	[PhoneNumberConfirmed], 
+	[SecurityStamp], 
+	[TwoFactorEnabled], 
+	[UserName]
+) 
+VALUES 
 (
-	@userId,
-	N'Administrator'
+	@userId, 
+	0, 
+	N'user3@falarseguros.pt', 
+	1, 
+	1, 
+	NULL, 
+	N'APyxnVe8q6Jg5S6N4QpDb8OYU5ommCD2rxv5NSXMRGvjdkSWZ9CZNT9Dr6/7ejADqQ==', 
+	NULL, 
+	0, 
+	N'c85f3301-03a8-42f1-b190-9d8469f1b652', 
+	0, 
+	N'user3@falarseguros.pt'
 )
+
+INSERT [dbo].[AspNetUserRoles] (RoleId,UserId) VALUES ('2',@userId)
+
+INSERT [Insurance].[Profile]([ID_User],[FirstName]) VALUES (@userId,N'User3')
+
+SET @userId = CONVERT(nvarchar(128), NEWID());
+
+INSERT [dbo].[AspNetUsers] (
+	[Id], 
+	[AccessFailedCount],
+	[Email], 
+	[EmailConfirmed], 
+	[LockoutEnabled], 
+	[LockoutEndDateUtc],  
+	[PasswordHash], 
+	[PhoneNumber], 
+	[PhoneNumberConfirmed], 
+	[SecurityStamp], 
+	[TwoFactorEnabled], 
+	[UserName]
+) 
+VALUES 
+(
+	@userId, 
+	0, 
+	N'user4@falarseguros.pt', 
+	1, 
+	1, 
+	NULL, 
+	N'APyxnVe8q6Jg5S6N4QpDb8OYU5ommCD2rxv5NSXMRGvjdkSWZ9CZNT9Dr6/7ejADqQ==', 
+	NULL, 
+	0, 
+	N'c85f3301-03a8-42f1-b190-9d8469f1b652', 
+	0, 
+	N'user4@falarseguros.pt'
+)
+
+INSERT [dbo].[AspNetUserRoles] (RoleId,UserId) VALUES ('2',@userId)
+
+INSERT [Insurance].[Profile]([ID_User],[FirstName]) VALUES (@userId,N'User4')
+
+SET @userId = CONVERT(nvarchar(128), NEWID());
+
+INSERT [dbo].[AspNetUsers] (
+	[Id], 
+	[AccessFailedCount],
+	[Email], 
+	[EmailConfirmed], 
+	[LockoutEnabled], 
+	[LockoutEndDateUtc],  
+	[PasswordHash], 
+	[PhoneNumber], 
+	[PhoneNumberConfirmed], 
+	[SecurityStamp], 
+	[TwoFactorEnabled], 
+	[UserName]
+) 
+VALUES 
+(
+	@userId, 
+	0, 
+	N'user5@falarseguros.pt', 
+	1, 
+	1, 
+	NULL, 
+	N'APyxnVe8q6Jg5S6N4QpDb8OYU5ommCD2rxv5NSXMRGvjdkSWZ9CZNT9Dr6/7ejADqQ==', 
+	NULL, 
+	0, 
+	N'c85f3301-03a8-42f1-b190-9d8469f1b652', 
+	0, 
+	N'user5@falarseguros.pt'
+)
+
+INSERT [dbo].[AspNetUserRoles] (RoleId,UserId) VALUES ('2',@userId)
+
+INSERT [Insurance].[Profile]([ID_User],[FirstName]) VALUES (@userId,N'User5')
--- InsuranceSocialNetwork/InsuranceWebsite/App_Start/MVCGridConfig.cs
+++ InsuranceSocialNetwork/InsuranceWebsite/App_Start/MVCGridConfig.cs
@@ -20,7 +20,7 @@ namespace InsuranceWebsite
         {
             MVCGridDefinitionTable.Add("UsersManagementGrid", new MVCGridBuilder<UserProfileModelObject>()
                 .WithAuthorizationType(AuthorizationType.AllowAnonymous)
-                .WithSorting(sorting: true, defaultSortColumn: "Id", defaultSortDirection: SortDirection.Dsc)
+                //.WithSorting(sorting: true, defaultSortColumn: "Id", defaultSortDirection: SortDirection.Dsc)
                 .WithPaging(paging: true, itemsPerPage: 10, allowChangePageSize: true, maxItemsPerPage: 100)
                 .WithAdditionalQueryOptionNames("search")
                 .AddColumns(cols =>
@@ -29,28 +29,72 @@ namespace InsuranceWebsite
                     cols.Add().WithColumnName("Username")
                         .WithHeaderText(Resources.Resources.Username)
                         .WithValueExpression(i => i.User.UserName)
+                        .WithAllowChangeVisibility(true)
+                        .WithFiltering(true)
+                        .WithSorting(true)
                         .WithVisibility(true, true); // use the Value Expression to return the cell text for this column
                     cols.Add().WithColumnName("FirstName")
                         .WithHeaderText(Resources.Resources.Name)
                         .WithValueExpression(i => i.FirstName)
+                        .WithAllowChangeVisibility(true)
+                        .WithFiltering(true)
+                        .WithSorting(true)
                         .WithVisibility(true, true); // use the Value Expression to return the cell text for this column
                     cols.Add().WithColumnName("UrlExample")
                         .WithHeaderText("Edit")
                         .WithValueExpression((i, c) => c.UrlHelper.Action("detail", "demo", new { id = i.ID }))
+                        .WithAllowChangeVisibility(true)
+                        .WithFiltering(true)
+                        .WithSorting(true)
                         .WithVisibility(true, true);
                 })
+                .WithSorting(true, "FirstName")
+                .WithAdditionalQueryOptionNames("Search")
                 .WithRetrieveDataMethod((context) =>
                 {
                     // Query your data here. Obey Ordering, paging and filtering parameters given in the context.QueryOptions.
                     // Use Entity Framework, a module from your IoC Container, or any other method.
                     // Return QueryResult object containing IEnumerable<YouModelItem>
 
+                    var options = context.QueryOptions;
+                    var result = new QueryResult<UserProfileModelObject>();
+                    //result.Items = BusinessItemsLists.GetUsers();
+                    var query = BusinessItemsLists.GetUsers();
+                    if (!String.IsNullOrWhiteSpace(options.SortColumnName))
+                    {
+                        switch (options.SortColumnName.ToLower())
+                        {
+                            case "firstname":
+                                if(options.SortDirection == SortDirection.Asc
+                                    || options.SortDirection == SortDirection.Unspecified)
+                                {
+                                    query = query.OrderBy(p => p.FirstName).ToList();
+                                }
+                                else
+                                {
+                                    query = query.OrderByDescending(p => p.FirstName).ToList();
+                                }
+                                
+                                break;
+                            case "lastname":
+                                //query = query.OrderBy(p => p.LastName, options.SortDirection);
+                                break;
+                        }
+                    }
+
+                    string globalSearch = options.GetAdditionalQueryOptionString("Search");
+                    query = query.Where(i => 
+                        i.FirstName.Contains(globalSearch)
+                        || i.LastName.Contains(globalSearch)
+                        || i.User.UserName.Contains(globalSearch)
+                        || i.ContactEmail.Contains(globalSearch)
+                    ).ToList();
+
                     return new QueryResult<UserProfileModelObject>()
                     {
-                        Items = BusinessItemsLists.GetUsers(),
-                        TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
+                        Items = query,
+                        TotalRecords = query.Count // if paging is enabled, return the total number of records of all pages
                     };
-
                 })
             );
         }
