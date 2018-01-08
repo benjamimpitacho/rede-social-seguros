3003311bd5892652be4e3184ce7ea9b7123be356
DevelopmentVM2\benjamim <benjamim_pitacho@hotmail.com>
2018-01-08 22:39:10 +0000

    * Change: email to allow emails to be sent. * Change: forgot and reset password view layouts.

--- InsuranceSocialNetwork/InsuranceWebsite/Controllers/AccountController.cs
+++ InsuranceSocialNetwork/InsuranceWebsite/Controllers/AccountController.cs
@@ -315,7 +315,7 @@ namespace InsuranceWebsite.Controllers
             System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(ConfigurationSettings.SmtpHost, ConfigurationSettings.SmtpPort)
             {
                 Credentials = new System.Net.NetworkCredential(ConfigurationSettings.SmtpUsername, ConfigurationSettings.SmtpPassword),
-                EnableSsl = true
+                EnableSsl = false
             };
             smtp.Send(m);
 
@@ -392,10 +392,10 @@ namespace InsuranceWebsite.Controllers
 
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
+                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // If we got this far, something failed, redisplay form
