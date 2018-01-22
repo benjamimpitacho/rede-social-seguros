5fcc5a826d90dc548168bd888e0387c1a8e465c7
DevelopmentVM2\benjamim <benjamim_pitacho@hotmail.com>
2018-01-22 23:42:13 +0000

    * Change: apply login layout style.

--- InsuranceSocialNetwork/InsuranceWebsite/Hubs/ChatHub.cs
+++ InsuranceSocialNetwork/InsuranceWebsite/Hubs/ChatHub.cs
@@ -23,8 +23,10 @@ namespace InsuranceWebsite.Hubs
         public void Send(string userId, string message, string chatId, string firstName, string lastName)
         {
             // Call the addNewMessageToPage method to update clients.
-            Clients.All.addNewMessageToPage(string.Format("{0} {1}", firstName, lastName), message, chatId);
-            //Clients.Client(chatId).sendPrivateMessage(name, name, message, name);
+            //Clients.All.addNewMessageToPage(string.Format("{0} {1}", firstName, lastName), message, chatId);
+            //Clients.Caller.addNewMessageToPage(string.Format("{0} {1}", firstName, lastName), message, chatId);
+            Clients.Client(Context.ConnectionId).addNewMessageToPage(string.Format("{0} {1}", firstName, lastName), message, chatId);
+            //Clients.Client(chatId).sendPrivateMessage(firstName, message);
             //Clients.Caller.sendPrivateMessage(chatId, name, message, chatId);
             InsuranceBusiness.BusinessLayer.SaveMessage(userId, chatId, message);
 
